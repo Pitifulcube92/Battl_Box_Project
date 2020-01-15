a5722c478452d766d075a33ddde1f4c736281706
Pitifulcube92 <nicodelgado1283>
2020-01-15 15:03:05 -0500

    d

--- Plugins/Battle_Box/Source/Battle_Box/Private/ActionClasses/AbilityAction.cpp
+++ Plugins/Battle_Box/Source/Battle_Box/Private/ActionClasses/AbilityAction.cpp
@@ -1,12 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AbilityClass.h"
+#include "AbilityAction.h"
 
-AbilityClass::AbilityClass()
+AbilityAction::AbilityAction()
 {
 }
 
-AbilityClass::~AbilityClass()
+AbilityAction::~AbilityAction()
 {
 }
