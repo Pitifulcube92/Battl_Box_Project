e04d6af307830532972b7a9e9df7533ee032432d
Pitifulcube92 <32850832+Pitifulcube92@users.noreply.github.com>
2020-01-19 13:03:17 -0500

    Made a second module for the runtime system

---
+++ Plugins/Battle_Box/Source/BB_Runtime_System/Private/SystemClasses/BaseSystem.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+#ifndef BASESYSTEM_H
+#define BASESYSTEM_H
+
+class BaseSystem
+{
+	virtual void BaseCalculate(const bool IsSingledTarget) = 0;
+};
+
+#endif // !BASESYSTEM_H
+
