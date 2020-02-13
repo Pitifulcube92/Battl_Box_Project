047e896c1beefe39a151089d48cfc57ab59272b7
Raphael Rodriguez <32069809+FailureRate@users.noreply.github.com>
2020-02-13 14:39:17 -0500

    f

--- Intermediate/ProjectFiles/ResourceLoader.cpp
+++ Intermediate/ProjectFiles/ResourceLoader.cpp
@@ -1,2 +1,36 @@
+<<<<<<< Updated upstream
 
 #include "ResourceLoader.h"
+=======
+#include "RESOURCELOADER.H"
+
+ResourceLoader::ResourceLoader()
+{
+	OnCreate();
+}
+
+ResourceLoader::~ResourceLoader()
+{
+	OnDestroy();
+}
+
+void ResourceLoader::DetermineLoadType()
+{
+}
+
+void ResourceLoader::OnCreate()
+{
+}
+
+void ResourceLoader::OnDestroy()
+{
+}
+
+void ResourceLoader::LoadActionSheet(const std::string & filename_)
+{
+}
+
+void ResourceLoader::LoadStatSheet(const std::string & filename_)
+{
+}
+>>>>>>> Stashed changes
