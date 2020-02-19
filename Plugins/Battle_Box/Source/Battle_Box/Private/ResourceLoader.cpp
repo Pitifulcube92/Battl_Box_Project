2ed1161a141bb508faa67f3be4ebad4b5bcc363d
Raphael Rodriguez <32069809+FailureRate@users.noreply.github.com>
2020-02-19 18:43:19 -0500

    ResourceLoader
    Fixed some issues

--- Plugins/Battle_Box/Source/Battle_Box/Private/ResourceLoader.cpp
+++ Plugins/Battle_Box/Source/Battle_Box/Private/ResourceLoader.cpp
@@ -1,5 +1,6 @@
 
 #include "ResourceLoader.h"
+std::unique_ptr<ResourceLoader> ResourceLoader::resourceInstance = nullptr;
 
 ResourceLoader::ResourceLoader()
 {
@@ -11,26 +12,82 @@ ResourceLoader::~ResourceLoader()
 	OnDestroy();
 }
 
-const BaseAction * ResourceLoader::GetAction(FString key_)
+BaseAction * ResourceLoader::ReturnAction(const FString name_)
 {
-	if (actionMap.Contains(key_)) {
-		return *actionMap.Find(key_);
+	if (actionMap.Contains(name_)) {
+		BaseAction* baseAction = new BaseAction(actionMap[name_]->ReturnName(), actionMap[name_]->ReturnDiscription, actionMap[name_]->ReturnActionType(), actionMap[name_]->ReturnTargetType(), actionMap[name_]->ReturnInteractionType(), actionMap[name_]->ReturnStatActionType, actionMap[name_]->ReturnActionID(), false);
+		return baseAction;
 	}
-	Debugger::Error("Key not found. Action not returned, returning nullptr", "ResourceLoader.cpp", __LINE__);
+	Debugger::SetSeverity(MessageType::E_ERROR);
+	Debugger::Error("Action not found in Resources returning Nullptr", "ResourceLoader.cpp", __LINE__);
 	return nullptr;
 }
 
-const StatSheetObject * ResourceLoader::GetStatSheetObject(FString key_)
+BaseAction * ResourceLoader::ReturnAction(const uint32 id_)
 {
-	if (statSheetMap.Contains(key_)) {
-		return *statSheetMap.Find(key_);
+	for (auto actions : actionMap) {
+		if (actions.Value->ReturnActionID() == id_) {
+			BaseAction * baseAction = new BaseAction(actions.Value);
+			return baseAction;
+		}
 	}
-	Debugger::Error("Key not found. Statsheet not returned, returning nullptr", "ResourceLoader.cpp", __LINE__);
+	Debugger::SetSeverity(MessageType::E_ERROR);
+	Debugger::Error("Action not found in Resources returning Nullptr", "ResourceLoader.cpp", __LINE__);
 	return nullptr;
 }
 
-void ResourceLoader::OnCreate()
+StatSheetObject * ResourceLoader::ReturnStatSheet(const FString name_)
 {
+	if (statSheetMap.Contains(name_)) {
+		StatSheetObject* statSheet = new StatSheetObject(statSheetMap[name_]->ReturnName(), statSheetMap[name_]->ReturnTag(), statSheetMap[name_]->ReturnCommand(), statSheetMap[name_]->ReturnItemMap(), statSheetMap[name_]->ReturnAbilityMap(), statSheetMap[name_]->ReturnStatMap(), , statSheetMap[name_]->ReturnEquipmentMap());
+		return statSheet;
+	}
+	Debugger::SetSeverity(MessageType::E_ERROR);
+	Debugger::Error("StatSheet object not found in Resources", "ResourceLoader.cpp", __LINE__);
+	return nullptr;
+}
+
+bool ResourceLoader::CheckAction(const FString name_)
+{
+	if (actionMap.Contains(name_)) {
+		return true;
+	}
+	Debugger::SetSeverity(MessageType::E_INFO);
+	Debugger::Info("Action of name: " + name_ + "does not exist in resoruces", "ResourceLoader.cpp", __LINE__);
+	return false;
+}
+
+bool ResourceLoader::CheckAction(const uint32 id_)
+{
+	for (auto actions : actionMap) {
+		if (actions.Value->ReturnActionID == id_) {
+			return true;
+		}
+	}
+	
+	Debugger::SetSeverity(MessageType::E_INFO);
+	Debugger::Info("Action of ID: "  "does not exist in resoruces", "ResourceLoader.cpp", __LINE__);
+	return false;
+}
+
+bool ResourceLoader::CheckStatSheet(const FString name_)
+{
+	if (statSheetMap.Contains(name_)) {
+		return true;
+	}
+	Debugger::SetSeverity(MessageType::E_INFO);
+	Debugger::Info("StatSheet of name: " + name_ + "does not exist in resoruces", "ResourceLoader.cpp", __LINE__);
+	return false;
+}
+
+
+void ResourceLoader::OnCreate(){
+	// create in order comand, abilty, items, then load into map.  
+	// use file manager
+
+	// 
+	// create stat sheet objects
+	// 
 }
 
 void ResourceLoader::OnDestroy()
@@ -38,33 +95,92 @@ void ResourceLoader::OnDestroy()
 	if (actionMap.Num > 0) {
 		for (auto actions : actionMap) {
 			delete actions.Value;
-			actions.Value = nullptr;
+			//actions.Value = nullptr;
 		}
 	}
 	if (statSheetMap.Num > 0) {
 		for (auto statSheet : statSheetMap) {
 			delete statSheet.Value;
-			statSheet.Value = nullptr;
+			//statSheet.Value = nullptr;
 		}
 	}
 }
 
-void ResourceLoader::LoadActionSheet(BaseAction* action_){
+ResourceLoader * ResourceLoader::GetInstance()
+{
+	if (resourceInstance == nullptr) {
+		resourceInstance.reset(new ResourceLoader);
+	}
+	return resourceInstance.get();
+}
+
+void ResourceLoader::AddAction(BaseAction * const action_)
+{
 	actionMap.Add(action_->ReturnName(), action_);
 }
 
-void ResourceLoader::LoadStatSheet(StatSheetObject* statSheet_){
-	if (statSheet_->ReturnName() == "") {
-		FString foo = "statSheet " + std::to_string(statSheetMap.Num())
-		statSheet_->SetName();
-	}
+void ResourceLoader::AddStatSheet(StatSheetObject * const statSheet_)
+{
 	statSheetMap.Add(statSheet_->ReturnName(), statSheet_);
 }
+
+void ResourceLoader::DeleteAction(const FString name_)
+{
+	if (actionMap.Contains(name_)) {
+		actionMap.Remove(name_);
+	}
+	else {
+		Debugger::Info("action Object doesn't exist by name", "ResourceLoader.cpp", __LINE__);
+	}
+}
+
+void ResourceLoader::DeleteStatSheet(const FString name_)
+{
+	if (statSheetMap.Contains(name_)) {
+		statSheetMap.Remove(name_);
+	}
+	else {
+		Debugger::Info("statSheet Object doesn't exist by name", "ResourceLoader.cpp", __LINE__);
+	}
+}
+
+void ResourceLoader::DeleteAction(const uint32 id_)
+{
+	for (auto actions : actionMap) {
+		if (actions.Value->ReturnActionID() == id_) {
+			actionMap.Remove(actions.Key);
+			break;
+		}
+	}
+	Debugger::Info("Action ID is not found in Action map", "ResrouceLoader.cpp", __LINE__);
+}
+
+//void ResourceLoader::AddAction(const FString name_)
+//{
+//	BaseAction* baseAction;// = /////// Will pull info from the Json Parser
+//
+//
+//
+//	if (baseAction->ReturnName() == "") { // if the Name is empty create name based on the amount in map
+//		FString newName = "action " + actionMap.Num();
+//		baseAction->SetName(newName);
+//		actionMap.Add(baseAction->ReturnName(), baseAction);
+//	}
+//	else if (!actionMap.Contains(baseAction->ReturnName())) { // statsheet name is not empty and does not contain the current name
+//		actionMap.Add(baseAction->ReturnName(), baseAction); // add to the statsheetmap
+//	}
+//	else { // if the name is in the map 
+//		Debugger::Error("Action Object exists with name " + baseAction->ReturnName(), "ResourceLoader.cpp", __LINE__);
+//		FString newName = "Action " + statSheetMap.Num();
+//		baseAction->SetName(newName);
+//		actionMap.Add(baseAction->ReturnName(), baseAction);
+//	}
+//}
 
 uint32 ResourceLoader::GetActionID(const std::string& key_)
 {
 	FString key = key_.c_str();//because the map is Fstring conversion must occur
-	BaseAction* base;
+	const BaseAction* base;
 	if((base = *actionMap.Find(key)) != nullptr){ // object is found at key 
 		return base->ReturnActionID(); // Return Action Key is call by the object then returned
 	}
--- Plugins/Battle_Box/Source/Battle_Box/Private/ResourceLoader.h
+++ Plugins/Battle_Box/Source/Battle_Box/Private/ResourceLoader.h
@@ -14,22 +14,50 @@ class StatSheetObject;
 
 class ResourceLoader
 {
-public:
-	static void LoadActionSheet(BaseAction* action_); // 
-	static void LoadStatSheet(StatSheetObject* statSheet_); // 
-	static const BaseAction* GetAction(FString key_);
-	static const StatSheetObject* GetStatSheetObject(FString key_);
-	void OnCreate();
+public:// Singleton stuff
+	ResourceLoader(const ResourceLoader&) = delete;
+	ResourceLoader(ResourceLoader&&) = delete;
+	ResourceLoader& operator=(const ResourceLoader&) = delete;
+	ResourceLoader& operator=(ResourceLoader&&) = delete;
+	// Singleton 
+	static ResourceLoader* GetInstance(); // The Instance of Resrouce Loader
+
+	// Will Add action based on name
+	static void AddAction( BaseAction* const action_);
+	// will add a statsheet based on name
+	static void AddStatSheet(StatSheetObject* conststatSheet_);
+	
+	// Deletes action by name 
+	static void DeleteAction(const FString name_); 
+	 // deletes statsheet by name
+	static void DeleteStatSheet(const FString name_);
+	// deletes action by it's ID
+	static void DeleteAction(const uint32 id_); 
+
+	// Returns Object if inside 
+	static BaseAction* ReturnAction(const FString name_);
+	// returns object by id
+	static BaseAction* ReturnAction(const uint32 id_); 
+	 // returns object by name
+	static StatSheetObject* ReturnStatSheet(const FString name_);
+
+	// this will check if object is in the map and return bool;
+	static bool CheckAction(const FString name_); 
+	// this will check if object is in the map by ID and return bool
+	static bool CheckAction(const uint32 id_);
+	// this will check if object is in the map and return bool
+	static bool CheckStatSheet(const FString name_);
+
+	void OnCreate(); 
 	void OnDestroy();
 
 private:
 	ResourceLoader();
 	~ResourceLoader();
-
-	static TMap<FString,const BaseAction*> actionMap;
-	static TMap<FString,const StatSheetObject*> statSheetMap;
+	static TMap<const FString, BaseAction* const> actionMap;
+	static TMap<const FString, StatSheetObject* const> statSheetMap;
 	static uint32 GetActionID(const std::string& key_);
-
+	static std::unique_ptr<ResourceLoader> resourceInstance;
 };
 
 #endif
